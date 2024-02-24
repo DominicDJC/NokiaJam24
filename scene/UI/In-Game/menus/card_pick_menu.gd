d40ce4245383fffb9d15bb53f7aa4896c7be409b
rretureau <romain.retureau@gmail.com>
2024-02-24 15:57:28 +0100

    Gameover screen and bind menus

--- scene/UI/In-Game/menus/card_pick_menu.gd
+++ scene/UI/In-Game/menus/card_pick_menu.gd
@@ -1,18 +1,52 @@
 extends Control
 
-signal picked_card
+signal picked_card(equipment_index: int)
 
 var in_menu = false
+var e_index: int = 0
+var current_card: Card = null
+var current_card_index = null
+var scrolling_cards: Array = []
+var inventory = []
 
-func open_menu():
+@onready var card_display = $CardPicker/Card
+@onready var card_description = $CardInfos/Description
+@onready var ptarget_section = $CardInfos/PrimaryTargets
+@onready var ptarget1 = $CardInfos/PrimaryTargets/EnemyIcon1
+@onready var ptarget2 = $CardInfos/PrimaryTargets/EnemyIcon2
+
+func open_menu(equipment_index):
+	e_index = equipment_index
+	inventory = Global.get_inventory()
+	current_card = Global.get_equipped()[e_index]
+	
+	if current_card == null:
+		current_card = Global.get_inventory()[0]
+		
+	current_card_index = inventory.find(current_card)
+	_set_current_card()
+	_initialize_card_list()
 	# Ugly but without this, return button is automatically pressed when entering menu
 	await get_tree().create_timer(0.1).timeout
 	in_menu = true
 	
+func _initialize_card_list():
+	# initialize list of possible card (do not show already equiped)
+	scrolling_cards = []
+	var all_inventory = Global.get_inventory()
+	var equiped = Global.get_equipped()
+	
+	for card in all_inventory:
+		if not (card in equiped) or card == current_card:
+			scrolling_cards.append(card)
+	
 func _process(_delta):
 	if in_menu:
 		if Input.is_action_just_pressed("key_0"):
-			picked_card.emit()
+			var equipped = Global.get_equipped()
+			equipped[e_index] = current_card
+			Global.set_equipped(equipped)
+			picked_card.emit(e_index)
 			in_menu = false
 
 		elif Input.is_action_just_pressed("key_6"):
@@ -21,9 +55,29 @@ func _process(_delta):
 		elif Input.is_action_just_pressed("key_4"):
 			_prev_card()
 			
-			
+
+func _set_current_card():
+	current_card = inventory[current_card_index]
+	card_display.texture = current_card.card_texture
+	card_description.text = current_card.description
+	
+	if current_card.type != "weapon":
+		ptarget_section.set_visible(false)
+	else:
+		ptarget_section.set_visible(true)
+		var enemies = current_card.get_damageable_enemies()
+		ptarget1.texture = enemies[0].icon_texture
+		ptarget2.texture = enemies[1].icon_texture
+		
 func _next_card():
-	pass
+	current_card_index += 1
+	if current_card_index >= len(inventory):
+		current_card_index = 0
+	_set_current_card()
 	
 func _prev_card():
-	pass
+	current_card_index -= 1
+	if current_card_index < 0:
+		len(inventory) - 1
+	_set_current_card()
+	
--- scene/UI/In-Game/menus/equipment_menu.gd
+++ scene/UI/In-Game/menus/equipment_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 signal close_menu 
-signal card_menu
+signal card_menu(equipment_idx: int)
 
 var in_menu: bool = false
 
@@ -9,12 +9,40 @@ var in_menu: bool = false
 @onready var slot1 = $Equipment/Slot1
 @onready var slot2 = $Equipment/Slot2
 @onready var slot3 = $Equipment/Slot3
+@onready var slots = [slot1, slot2, slot3]
 
-func open_menu():
-	return_button.grab_focus()
+@onready var no_card_texture = preload("res://assets/img/UI/In-Game/cards/cards_nocard.png")
+
+func _ready():
+	_refresh_menu()
+
+func _refresh_menu():
+	var equiped = Global.get_equipped()
+	
+	for i in range(3):
+		if equiped[i] != null:
+			slots[i].texture_normal = equiped[i].card_texture
+		else:
+			slots[i].texture_normal = no_card_texture
+	
+func open_menu(equipment_index = 0):
+	slots[equipment_index].grab_focus()
+	
+	_refresh_menu()
 	# Ugly but without this, return button is automatically pressed when entering menu
 	await get_tree().create_timer(0.1).timeout
 	in_menu = true
+
+func _can_open():
+	var n_equipped = 0
+	for equipment in Global.get_equipped():
+		if equipment != null:
+			n_equipped += 1
+	
+	if n_equipped >= len(Global.get_inventory()):
+		return false
+	
+	return true
 
 func _process(_delta):
 	if in_menu:
@@ -26,16 +54,19 @@ func _process(_delta):
 					in_menu = false
 				
 				slot1:
-					card_menu.emit("card1")
-					in_menu = false
+					if _can_open():
+						card_menu.emit(0)
+						in_menu = false
 					
 				slot2:
-					card_menu.emit("card1")
-					in_menu = false
+					if _can_open():
+						card_menu.emit(1)
+						in_menu = false
 					
 				slot3:
-					card_menu.emit("card1")
-					in_menu = false
+					if _can_open():
+						card_menu.emit(2)
+						in_menu = false
 		
 		elif Input.is_action_just_pressed("key_6"):
 			var neighbor = focus_owner.find_valid_focus_neighbor(SIDE_RIGHT)
@@ -64,18 +95,14 @@ func _on_slot_1_focus_entered():
 func _on_slot_1_focus_exited():
 	card_unfocused(slot1)
 
-
 func _on_slot_2_focus_entered():
 	card_focused(slot2)
-
 
 func _on_slot_2_focus_exited():
 	card_unfocused(slot2)
 
-
 func _on_slot_3_focus_entered():
 	card_focused(slot3)
-
 
 func _on_slot_3_focus_exited():
 	card_unfocused(slot3)
